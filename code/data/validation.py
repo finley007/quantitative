4c72ac2a799252aaab4421e9f4d0b0d6371580a9
finley <finley007@163.com>
2022-10-17 00:32:18 +0800

    增加数据校验类

--- code/data/validation.py
+++ code/data/validation.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python
 # -*- coding:utf8 -*-
+import re
 from abc import ABCMeta, abstractmethod
+import hashlib
+
+import pandas as pd
 
 from common.exception.exception import ValidationFailed
+from common.io import read_decompress, list_files_in_path
+from tools.tools import extract_tsccode
 
 
 #和金字塔数据源比对
@@ -54,7 +60,6 @@ class StockFilterValidator(Validator):
     """
 
     @classmethod
-    @abstractmethod
     def validate(self, data):
         """数据验证接口
         1. 检查成交量是否递增
@@ -68,7 +73,6 @@ class StockFilterValidator(Validator):
         print(data)
 
     @classmethod
-    @abstractmethod
     def compare_validate(self, target_data, compare_data):
         """数据比较验证接口
         1. 数量
@@ -81,5 +85,33 @@ class StockFilterValidator(Validator):
             对比数据.
         """
         if (len(target_data) != len(compare_data)):
-            raise ValidationFailed('Invalid data length')
+            raise ValidationFailed('Different data length')
+        target_sign_list = [hashlib.md5('|'.join(item).encode('gbk')).hexdigest() for item in [str(item) for item in target_data.values.tolist()]]
+        compare_sign_list = [hashlib.md5('|'.join(item).encode('gbk')).hexdigest() for item in [str(item) for item in target_data.values.tolist()]]
+        diff = list(set(target_sign_list).difference(set(compare_sign_list)))
+        diff.extend(list(set(compare_sign_list).difference(set(target_sign_list))))
+        if diff != []:
+            raise ValidationFailed('Different content')
+        return True
+
+
+
+
+
+if __name__ == '__main__':
+    month_folder_path = '/Users/finley/Projects/stock-index-future/data/original/stock_daily/stk_tick10_w_2017/stk_tick10_w_201701/'
+    date = '20170103'
+    stock_file_list = list_files_in_path(month_folder_path + '/' + date)
+    for stock_file in stock_file_list:
+        stock = extract_tsccode(stock_file)
+        if not re.match('[0-9]{6}', stock):
+            continue
+        print('Validate for stock %s' % stock)
+        data_csv = pd.read_csv(month_folder_path + '/' + date + '/' + stock_file, encoding='gbk')
+        try:
+            data_pkl = read_decompress(month_folder_path + '/' + date + '/pkl/' +  stock + '.pkl')
+        except Exception as e:
+            continue
+        if StockFilterValidator().compare_validate(data_pkl, data_csv):
+            print('Validation pass for stock %s' % stock)
 
