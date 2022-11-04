133cd73986a607fee36b1dc06debe2673d8a9808
finley <finley007@163.com>
2022-11-04 15:48:44 +0800

    增加因子基类

---
+++ code/factor/factor.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+from abc import ABCMeta, abstractclassmethod, abstractmethod
+
+from scipy.stats import pearsonr
+
+
+class Factor(metaclass=ABCMeta):
+    """因子基类
+
+    Parameters
+    ----------
+    factor_code : string
+    version : string
+    _params ： list
+    """
+    factor_code = ''
+    version = '1.0'
+
+    _params = []
+
+    def get_params(self):
+        return self._params
+
+    # 两个因子相关性分析
+    def compare(self, factor, data):
+        data = self.caculate(data)
+        data = factor.caculate(data)
+        data = data.dropna()
+        return pearsonr(data[self.get_factor_code()], data[factor.get_factor_code()])
+
+    # 全局计算因子值
+    @abstractmethod
+    @classmethod
+    def caculate(self, data):
+        pass
