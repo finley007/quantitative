968b66795aec9da78abc4cf6d39ba1379a7d1e6d
Finley <finley007@163.com>
2022-11-25 10:29:11 +0800

    处理威廉因子分母为0的情况

--- code/factor/volume_price_factor.py
+++ code/factor/volume_price_factor.py
@@ -28,11 +28,12 @@ class WilliamFactor(Factor):
     def caculate(self, data):
         for param in self._params:
             data[self.get_key(param)] = (data['close'] - (data['high'].rolling(window=param).max()+data['low'].rolling(window=param).min())/2)/(data['high'].rolling(window=param).max()-data['low'].rolling(window=param).min())
+            data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
         return data
 
 if __name__ == '__main__':
     #测试威廉因子
-    william_factor = WilliamFactor([1000])
+    william_factor = WilliamFactor([10])
     print(william_factor.factor_code)
     print(william_factor.version)
     print(william_factor.get_params())
@@ -41,14 +42,17 @@ if __name__ == '__main__':
     print(william_factor.get_key(5))
     print(william_factor.get_keys())
 
-    # data = read_decompress(TEST_PATH + '20200928.pkl')
-    # print(william_factor.caculate(data)[0:50][['close'] + william_factor.get_keys()])
-    # data.index = pd.DatetimeIndex(data['datetime'])
-    # data = data[(data['datetime'] >= '2020-09-28 10:00:00') & (data['datetime'] <= '2020-09-28 10:30:00')]
+    data = read_decompress(TEST_PATH + 'IC2003.pkl')
+    data = william_factor.caculate(data)
+    print(data.loc[776842:776844])
+    # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
+    # print(data[['close', 'datetime'] + william_factor.get_keys()])
+    data.index = pd.DatetimeIndex(data['datetime'])
     # draw_analysis_curve(data, show_signal=True, signal_keys=william_factor.get_keys())
 
-    data = william_factor.load()
-    print(data)
+    # 测试加载数据
+    # data = william_factor.load()
+    # print(data)
 
 
 
