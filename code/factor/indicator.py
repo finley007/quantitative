edd3b9d3a9302ca69fd40ae47769f4be111041d1
finley <finley007@163.com>
2022-11-29 14:21:52 +0800

    增加多项式回归和标准差算子及相关因子

--- code/factor/indicator.py
+++ code/factor/indicator.py
@@ -2,6 +2,7 @@
 # -*- coding:utf8 -*-
 from abc import ABCMeta, abstractmethod
 from sklearn.linear_model import LinearRegression as lr
+from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 
 from common.localio import read_decompress
@@ -78,6 +79,23 @@ class ExpMovingAverage(Indicator):
             data[self.get_key(param)] = data[self._target].ewm(alpha=2 / (param + 1), adjust=False).mean()
         return data
 
+class StandardDeviation(Indicator):
+    """
+    标准差
+    """
+    key = 'standard_deviation'
+
+    def __init__(self, params, target='close'):
+        self._params = params
+        self._target = target
+
+    def get_key(self, param):
+        return self.key + '.' + self._target + '.' + str(param)
+
+    def enrich(self, data):
+        for param in self._params:
+            data[self.get_key(param)] = data[self._target].rolling(param).std()
+        return data
 
 class ATR(Indicator):
     """
@@ -117,37 +135,78 @@ class LinearRegression(Indicator):
         self._params = params
         self._target = target
 
-    def set_key_func(self, func):
-        self._key_func = func
+    def set_caculation_func(self, func):
+        self._caculation_func = func
 
     def get_key(self, param):
         return self.key + '.' + self._target + '.' + str(param)
 
     def enrich(self, data):
         for param in self._params:
-            data[self.get_key(param)] = data[self._target].rolling(param).apply(lambda x: self.regression(x))
+            data[self.get_key(param)] = data[self._target].rolling(param).apply(lambda x: self.regression(x, self._caculation_func))
         return data
 
-    def regression(self, window):
+    def regression(self, window, func):
         x = np.linspace(0, 1, len(window)).reshape(-1, 1)
         y = np.array(window.tolist()).reshape(-1, 1)
         self.model.fit(x, y)
-        return self.model.coef_
+        return func(self.model, window)
+
+class PolynomialRegression(Indicator):
+    """
+    多项式回归
+    """
+
+    key = 'polynomial_regression'
+
+    model = lr()
+
+    def __init__(self, params, target = 'close', order = 2):
+        self._params = params
+        self._target = target
+        self.poly = PolynomialFeatures(order)
+
+    def set_caculation_func(self, func):
+        self._caculation_func = func
+
+    def get_key(self, param):
+        return self.key + '.' + self._target + '.' + str(param)
+
+    def enrich(self, data):
+        for param in self._params:
+            data[self.get_key(param)] = data[self._target].rolling(param).apply(lambda x: self.regression(x, self._caculation_func))
+        return data
+
+    def regression(self, window, func):
+        x = np.linspace(0, 1, len(window)).reshape(-1, 1)
+        self.poly.fit(x.reshape(-1, 1))
+        X = self.poly.transform(x.reshape(-1, 1))
+        y = np.array(window.tolist()).reshape(-1, 1)
+        self.model.fit(X, y)
+        return func(self.model, self.poly, window)
+
 
 if __name__ == '__main__':
     data = read_decompress('/Users/finley/Projects/stock-index-future/data/organised/future/IH/IH2209.pkl')
     data = data[(data['datetime'] >= '2022-01-24 09:30:00') & (data['datetime'] <= '2022-01-24 10:30:00')]
-    data = MovingAverage([10]).enrich(data)
+    # data = MovingAverage([10]).enrich(data)
+    # print(data)
+    #
+    # data = ExpMovingAverage([10]).enrich(data)
+    # print(data)
+    #
+
+    data = StandardDeviation([10]).enrich(data)
     print(data)
 
-    data = ExpMovingAverage([10]).enrich(data)
-    print(data)
+    # data = ATR([10]).enrich(data)
+    # print(data)
+    #
+    # data = LinearRegression([10]).enrich(data)
+    # print(data)
 
-    data = ATR([10]).enrich(data)
-    print(data)
-
-    data = LinearRegression([10]).enrich(data)
-    print(data)
+    # data = PolynomialRegression([10]).enrich(data)
+    # print(data)
 
 
 
--- code/factor/volume_price_factor.py
+++ code/factor/volume_price_factor.py
@@ -4,12 +4,12 @@ import math
 from math import sqrt
 
 import pandas as pd
+import numpy as np
 
 from common.visualization import draw_analysis_curve
 from factor.base_factor import Factor
-from common.constants import TEST_PATH
 from common.localio import read_decompress
-from factor.indicator import ATR, MovingAverage, LinearRegression
+from factor.indicator import ATR, MovingAverage, LinearRegression, PolynomialRegression, StandardDeviation
 
 """量价类因子
 分类编号：01
@@ -78,6 +78,7 @@ class LinearPerAtrFactor(Factor):
         self._params = params
         self._atr = ATR(self._params)
         self._linear_regression = LinearRegression(self._params, 'log')
+        self._linear_regression.set_key_func(self.caculation_function)
 
     def caculate(self, data):
         data['log'] = data.apply(lambda item:math.log((item['open'] + item['close'] + item['high'] + item['low'])/4), axis=1)
@@ -87,6 +88,130 @@ class LinearPerAtrFactor(Factor):
             data[self.get_key(param)] = data[self._linear_regression.get_key(param)]/data[self._atr.get_key(param)]
             data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
         return data
+
+    def caculation_function(self, model, window):
+        return model.coef_
+
+class LinearDeviationFactor(Factor):
+    """
+    TSSB LINEAR DEVIATION HistLength 因子
+    """
+
+    factor_code = 'FCT_01_003_LINEAR_DEVIATION'
+    version = '1.0'
+
+    def __init__(self, params = [10, 20, 50, 100]):
+        self._params = params
+        self._linear_regression = LinearRegression(self._params)
+        self._linear_regression.set_caculation_func(self.caculation_function)
+
+    def caculate(self, data):
+        data = self._linear_regression.enrich(data)
+        for param in self._params:
+            data[self.get_key(param)] = data[self._linear_regression.get_key(param)]
+            data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
+        return data
+
+    def caculation_function(self, model, window):
+        x = np.linspace(0, 1, len(window)).reshape(-1, 1)
+        estimated_values = model.predict(x)
+        estimated_value = estimated_values[-1]
+        true_value = window.tolist()[-1]
+        return (true_value - estimated_value) / self.get_standard_error(window.tolist(), estimated_values)
+
+    def get_standard_error(self, true_values, estimated_values):
+        n = len(true_values)
+        return ((((np.array(true_values) - np.array(estimated_values))**2).sum() / (n - 1)) ** 0.5) / n ** 0.5 #stardand error等于标准差除以根号n
+
+class QuadraticDeviationFactor(Factor):
+    """
+    TSSB QUADRATIC DEVIATION HistLength 因子
+    """
+
+    factor_code = 'FCT_01_004_QUADRATIC_DEVIATION'
+    version = '1.0'
+
+    def __init__(self, params = [10, 20, 50, 100]):
+        self._params = params
+        self._polynomial_regression = PolynomialRegression(self._params)
+        self._polynomial_regression.set_caculation_func(self.caculation_function)
+
+    def caculate(self, data):
+        data = self._polynomial_regression.enrich(data)
+        for param in self._params:
+            data[self.get_key(param)] = data[self._polynomial_regression.get_key(param)]
+            data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
+        return data
+
+    def caculation_function(self, model, poly, window):
+        x = np.linspace(0, 1, len(window)).reshape(-1, 1)
+        X = poly.transform(x.reshape(-1, 1))
+        estimated_values = model.predict(X)
+        estimated_value = estimated_values[-1]
+        true_value = window.tolist()[-1]
+        return (true_value - estimated_value) / self.get_standard_error(window.tolist(), estimated_values)
+
+    def get_standard_error(self, true_values, estimated_values):
+        n = len(true_values)
+        return ((((np.array(true_values) - np.array(estimated_values))**2).sum() / (n - 1)) ** 0.5) / n ** 0.5 #stardand error等于标准差除以根号n
+
+class CubicDeviationFactor(Factor):
+    """
+    TSSB CUBIC DEVIATION HistLength 因子
+    """
+
+    factor_code = 'FCT_01_005_CUBIC_DEVIATION'
+    version = '1.0'
+
+    def __init__(self, params = [10, 20, 50, 100]):
+        self._params = params
+        self._polynomial_regression = PolynomialRegression(self._params, order = 3) #三阶多项式
+        self._polynomial_regression.set_caculation_func(self.caculation_function)
+
+    def caculate(self, data):
+        data = self._polynomial_regression.enrich(data)
+        for param in self._params:
+            data[self.get_key(param)] = data[self._polynomial_regression.get_key(param)]
+            data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
+        return data
+
+    def caculation_function(self, model, poly, window):
+        x = np.linspace(0, 1, len(window)).reshape(-1, 1)
+        X = poly.transform(x.reshape(-1, 1))
+        estimated_values = model.predict(X)
+        estimated_value = estimated_values[-1]
+        true_value = window.tolist()[-1]
+        return (true_value - estimated_value) / self.get_standard_error(window.tolist(), estimated_values)
+
+    def get_standard_error(self, true_values, estimated_values):
+        n = len(true_values)
+        return ((((np.array(true_values) - np.array(estimated_values))**2).sum() / (n - 1)) ** 0.5) / n ** 0.5 #stardand error等于标准差除以根号n
+
+class PriceMomentumFactor(Factor):
+    """
+    TSSB PRICE MOMENTUM HistLength StdDevLength 因子
+    """
+
+    factor_code = 'FCT_01_006_PRICE_MOMENTUM'
+    version = '1.0'
+
+    def __init__(self, params = [10, 20, 50, 100]):
+        self._params = params
+        self._standard_deviation = StandardDeviation(list(map(lambda x: 2*x, self._params)), 'log2')
+
+    def caculate(self, data):
+        data['mean'] = (data['open'] + data['close'] + data['high'] + data['low'])/4
+        data['log2'] = data['mean'].rolling(2).apply(lambda item: self.formula(item))
+        data = self._standard_deviation.enrich(data)
+        for param in self._params:
+            data['log' + str(param)] = data['mean'].rolling(param).apply(lambda item: self.formula(item))
+            data[self.get_key(param)] = data['log' + str(param)]/(data[self._standard_deviation.get_key(2*param)]*(param**0.5))
+            data.loc[data[self.get_key(param)].isnull(), self.get_key(param)] = 0
+        return data
+
+    def formula(self, item):
+        list = item.tolist()
+        return math.log(list[-1]/list[0])
 
 
 if __name__ == '__main__':
@@ -123,18 +248,75 @@ if __name__ == '__main__':
     # draw_analysis_curve(data, show_signal=True, signal_keys=close_minus_moving_average_factor.get_keys())
 
     # TSSB LINEAR PER ATR HistLength ATRlength
-    linear_per_atr_factor = LinearPerAtrFactor([10])
-    print(linear_per_atr_factor.factor_code)
-    print(linear_per_atr_factor.version)
-    print(linear_per_atr_factor.get_params())
-    print(linear_per_atr_factor.get_category())
-    print(linear_per_atr_factor.get_full_name())
-    print(linear_per_atr_factor.get_key(5))
-    print(linear_per_atr_factor.get_keys())
+    # linear_per_atr_factor = LinearPerAtrFactor([10])
+    # print(linear_per_atr_factor.factor_code)
+    # print(linear_per_atr_factor.version)
+    # print(linear_per_atr_factor.get_params())
+    # print(linear_per_atr_factor.get_category())
+    # print(linear_per_atr_factor.get_full_name())
+    # print(linear_per_atr_factor.get_key(5))
+    # print(linear_per_atr_factor.get_keys())
+    # # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
+    # data = linear_per_atr_factor.caculate(data)
+    # data.index = pd.DatetimeIndex(data['datetime'])
+    # draw_analysis_curve(data, show_signal=True, signal_keys=linear_per_atr_factor.get_keys())
+
+    # TSSB LINEAR DEVIATION HistLength
+    # linear_deviation_factor = LinearDeviationFactor([10])
+    # print(linear_deviation_factor.factor_code)
+    # print(linear_deviation_factor.version)
+    # print(linear_deviation_factor.get_params())
+    # print(linear_deviation_factor.get_category())
+    # print(linear_deviation_factor.get_full_name())
+    # print(linear_deviation_factor.get_key(5))
+    # print(linear_deviation_factor.get_keys())
+    # # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
+    # data = linear_deviation_factor.caculate(data)
+    # data.index = pd.DatetimeIndex(data['datetime'])
+    # draw_analysis_curve(data, show_signal=True, signal_keys=linear_deviation_factor.get_keys())
+
+    # TSSB LINEAR DEVIATION HistLength
+    # quadratic_deviation_factor = QuadraticDeviationFactor([10])
+    # print(quadratic_deviation_factor.factor_code)
+    # print(quadratic_deviation_factor.version)
+    # print(quadratic_deviation_factor.get_params())
+    # print(quadratic_deviation_factor.get_category())
+    # print(quadratic_deviation_factor.get_full_name())
+    # print(quadratic_deviation_factor.get_key(5))
+    # print(quadratic_deviation_factor.get_keys())
+    # # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
+    # data = quadratic_deviation_factor.caculate(data)
+    # data.index = pd.DatetimeIndex(data['datetime'])
+    # draw_analysis_curve(data, show_signal=True, signal_keys=quadratic_deviation_factor.get_keys())
+
+    # TSSB LINEAR DEVIATION HistLength
+    # cubic_deviation_factor = CubicDeviationFactor([10])
+    # print(cubic_deviation_factor.factor_code)
+    # print(cubic_deviation_factor.version)
+    # print(cubic_deviation_factor.get_params())
+    # print(cubic_deviation_factor.get_category())
+    # print(cubic_deviation_factor.get_full_name())
+    # print(cubic_deviation_factor.get_key(5))
+    # print(cubic_deviation_factor.get_keys())
+    # # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
+    # data = cubic_deviation_factor.caculate(data)
+    # data.index = pd.DatetimeIndex(data['datetime'])
+    # draw_analysis_curve(data, show_signal=True, signal_keys=cubic_deviation_factor.get_keys())
+
+    # TSSB PRICE MOMENTUM HistLength StdDevLength
+    price_momentum_factor = PriceMomentumFactor([10])
+    print(price_momentum_factor.factor_code)
+    print(price_momentum_factor.version)
+    print(price_momentum_factor.get_params())
+    print(price_momentum_factor.get_category())
+    print(price_momentum_factor.get_full_name())
+    print(price_momentum_factor.get_key(5))
+    print(price_momentum_factor.get_keys())
     # data = data[(data['datetime'] >= '2019-08-28 13:45:00') & (data['datetime'] <= '2019-08-28 13:48:00')]
-    data = linear_per_atr_factor.caculate(data)
+    print(data.iloc[0:10])
+    data = price_momentum_factor.caculate(data)
     data.index = pd.DatetimeIndex(data['datetime'])
-    draw_analysis_curve(data, show_signal=True, signal_keys=linear_per_atr_factor.get_keys())
+    draw_analysis_curve(data, show_signal=True, signal_keys=price_momentum_factor.get_keys())
 
     # 测试加载数据
     # data = william_factor.load()
