cb47b50ea2a4b04486d52620e214ba0afa80a335
Finley <finley007@163.com>
2022-12-05 14:43:26 +0800

    增加单元测试

---
+++ code/test/factor/test_indicator.py
@@ -0,0 +1,49 @@
+import pytest
+
+def test_moving_average():
+    print('test_moving_average')
+
+def test_exp_moving_average():
+    print('test_exp_moving_average')
+
+def test_weighted_moving_average():
+    print('test_weighted_moving_average')
+
+def test_standard_deviation():
+    print('test_standard_deviation')
+
+def test_variance():
+    print('test_variance')
+
+def test_skewness():
+    print('test_skewness')
+
+def test_kurtosis():
+    print('test_kurtosis')
+
+def test_median():
+    print('test_median')
+
+def test_quantile():
+    print('test_quantile')
+
+def test_tr():
+    print('test_tr')
+
+def test_atr():
+    print('test_atr')
+
+def test_linear_regression():
+    print('test_linear_regression')
+
+def test_polynomial_regression():
+    print('test_polynomial_regression')
+
+def test_adx():
+    print('test_adx')
+
+def test_obv():
+    print('test_obv')
+
+def test_rsi():
+    print('test_rsi')
